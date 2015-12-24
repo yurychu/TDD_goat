6141e0b1854d93b1f4539f829b0e59556e1a6bac
yurch <tesewar@mail.ru>
2015-12-24 11:47:36 +0300

    action в шаблонах

--- lists/views.py
+++ lists/views.py
@@ -6,9 +6,6 @@ from lists.models import Item
 # Create your views here.
 
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
     return render(request, 'home.html')
 
 def view_list(request):
