8de83e730726089c5fb173b545f31951eb79bbf9
yurch <tesewar@mail.ru>
2015-12-25 11:33:04 +0300

    Первые шаги нового теста расположения элементов на странице

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -101,6 +101,28 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Если эти оба останутся довольными, то идем спать.
 
+    def test_layout_and_styling(self):
+        # Эдит заходит на домашнюю страницу
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # Она видит строку ввода красиво расположенной по центру
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+                inputbox.location['x'] + inputbox.size['width'] / 2,
+                512,
+                delta=5
+                )
+        # Она начинает создавать новый список дел и при добавлени последующих
+        # так же видит строку ввода, расположенную по центру
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+                inputbox.location['x'] + inputbox.size['width'] / 2,
+                512,
+                delta=5
+                )
+
 if __name__ == "__main__":
     unittest.main(warnings='ignore')
 
--- lists/models.py
+++ lists/models.py
@@ -11,4 +11,3 @@ class Item(models.Model):
     text = models.TextField(default='')
     list = models.ForeignKey(List, default=None)
 
-
