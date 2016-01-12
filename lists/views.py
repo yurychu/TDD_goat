cbfccef700312c75e16b6bc103bac240842a1eeb
yurch <tesewar@mail.ru>
2016-01-12 14:52:15 +0300

    сохранение формы

--- lists/views.py
+++ lists/views.py
@@ -18,7 +18,7 @@ def view_list(request, list_id):
     if request.method == 'POST':
         form = ItemForm(data=request.POST)
         if form.is_valid():
-            Item.objects.create(text=request.POST['text'], list=list_)
+            form.save(for_list=list_)
             return redirect(list_)
     return render(request, 'list.html', {'list': list_, 'form': form})
 
