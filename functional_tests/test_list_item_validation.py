8b4b7aff7a4ea606ecbf45b300a789ddd1671b5a
yurch <tesewar@mail.ru>
2016-01-13 10:44:10 +0300

    добавление функционального теста валидации на стороне клиента

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -4,6 +4,9 @@ from .base import FunctionalTest
 
 class ItemValidatorTest(FunctionalTest):
 
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
+
     def test_cannot_add_empty_list_items(self):
         # Эдит заходит на домашнюю страницу и случайно пытается добавить пустой
         # елемент в список.
@@ -12,7 +15,7 @@ class ItemValidatorTest(FunctionalTest):
 
         # Домашняя страница обновляется и появляется сообщение об ошибке ввода
         # пустого значения. Говорится, что список элементов не может быть пустым.
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text, "You can't have an empty list item")
 
         # Она снова пытается добавить элемент в существуюем списке,
@@ -25,7 +28,7 @@ class ItemValidatorTest(FunctionalTest):
 
         # Но получает аналогичное предупреждение на странице со списком.
         self.check_for_row_in_list_table('1.Buy milk')
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text, "You can't have an empty list item")
 
         # Исправляет на корректный текст
@@ -45,6 +48,21 @@ class ItemValidatorTest(FunctionalTest):
 
         # Она видит подсказку об ошибке
         self.check_for_row_in_list_table('1.Buy wellies')
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text, "You've already got this in your list")
+
+    def test_errors_messages_are_cleared_on_input(self):
+        # Эдит начинает создавать новый список добавлением пустого элемета
+        # Должна быть вызвана ошибка валидации
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('\n')
+        error = self.get_error_element()
+        self.assertTrue(error.is_displayed())
+
+        # Она начинает печатать в поле ввода, что бы устранить ошибку
+        self.get_item_input_box().send_keys('a')
+
+        # Она в восторге, когда сообщение об ошибке исчезает
+        error = self.get_error_element()
+        self.assertFalse(error.is_displayed())
 
