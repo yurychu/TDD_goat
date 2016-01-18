8c969a7dec37cb608b1bb6d8f548ee5c9bae55b6
yurch <tesewar@mail.ru>
2016-01-18 14:59:35 +0300

    мелкие поправки

--- accounts/views.py
+++ accounts/views.py
@@ -1,18 +1,20 @@
 import sys
 
 from django.contrib.auth import authenticate
-from django.contrib.auth import login as auth_login
+from django.contrib.auth import login as auth_login, logout as auth_logout
 from django.shortcuts import redirect
+
 
 def login(request):
     print('login view', file=sys.stderr)
 
-    #user = PersonaAuthenticationBackend().authenticate(request.POST['assertion'])
+    # user = PersonaAuthenticationBackend().authenticate(request.POST['assertion'])
     user = authenticate(assertion=request.POST['assertion'])
 
     if user is not None:
         auth_login(request, user)
     return redirect('/')
+
 
 def logout(request):
     auth_logout(request)
