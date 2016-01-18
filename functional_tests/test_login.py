8490ff5b8ee304947876aa5b4d0f52ba825bdd74
yurch <tesewar@mail.ru>
2016-01-18 15:16:57 +0300

    start housekeeping@

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -27,7 +27,7 @@ class LoginTest(FunctionalTest):
         # Эдит заходит на наш замечательный сайт со списками
         # и впервые замечает сслыку "Sign in"
         self.browser.get(self.server_url)
-        self.browser.find_element_by_id('login').click()
+        self.browser.find_element_by_id('id_login').click()
 
         # Открывается окно логина Mozilla Persona
         self.switch_to_new_window("Mozilla Persona")
@@ -43,7 +43,7 @@ class LoginTest(FunctionalTest):
         self.switch_to_new_window('To-Do')
 
         # Она может увидеть, что она зашла
-        self.wait_for_element_with_id('logout')
+        self.wait_for_element_with_id('id_logout')
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertIn('edith@mockmyid.com', navbar.text)
 
