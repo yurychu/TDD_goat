0c0177aabadd34a05c55bd5263415335ac43d9f7
yurch <tesewar@mail.ru>
2016-01-25 15:14:33 +0300

    подстановка сессии без ручного логина в систему

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -4,6 +4,8 @@ from .base import FunctionalTest
 
 from selenium.webdriver.support.ui import WebDriverWait
 
+TEST_EMAIL = 'edith@mockmyid.com'
+
 
 class LoginTest(FunctionalTest):
 
@@ -18,24 +20,6 @@ class LoginTest(FunctionalTest):
             time.sleep(0.5)
         self.fail('could not find window')
 
-    def wait_for_element_with_id(self, element_id):
-        WebDriverWait(self.browser, timeout=30).until(
-            lambda b: b.find_element_by_id(element_id),
-            'Could not find element with id {}.Page text was:\n{}'.format(
-                element_id, self.browser.find_element_by_tag_name('body').text
-            )
-        )
-
-    def wait_to_be_logged_in(self):
-        self.wait_for_element_with_id('id_logout')
-        navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertIn('edith@mockmyid.com', navbar.text)
-
-    def wait_to_be_logged_out(self):
-        self.wait_for_element_with_id('id_login')
-        navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertNotIn('edith@mockmyid.com', navbar.text)
-
     def test_login_with_persona(self):
         # Эдит заходит на наш замечательный сайт со списками
         # и впервые замечает сслыку "Sign in"
@@ -49,25 +33,25 @@ class LoginTest(FunctionalTest):
         ## Используем mockmyid.com для теста
         self.browser.find_element_by_id(
             'authentication_email'
-        ).send_keys('edith@mockmyid.com')
+        ).send_keys(TEST_EMAIL)
         self.browser.find_element_by_tag_name('button').click()
 
         # Окно Persona закрывается
         self.switch_to_new_window('To-Do')
 
         # Она может увидеть, что она зашла
-        self.wait_to_be_logged_in()
+        self.wait_to_be_logged_in(email=TEST_EMAIL)
 
         # Обновление страницы, реальная ссесия должна сохраниться
         # и что не произошло выхода со страницы
         self.browser.refresh()
-        self.wait_to_be_logged_in()
+        self.wait_to_be_logged_in(email=TEST_EMAIL)
 
         # Испугавшись новой фичи, жмет logout
         self.browser.find_element_by_id('id_logout').click()
-        self.wait_to_be_logged_out()
+        self.wait_to_be_logged_out(email=TEST_EMAIL)
 
         # Статус logged out - должен сохраняться после рефреша страницы
         self.browser.refresh()
-        self.wait_to_be_logged_out()
+        self.wait_to_be_logged_out(email=TEST_EMAIL)
 
