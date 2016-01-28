f34efe6abd4006d16ea23ba54422ba8079b75e9d
yurch <tesewar@mail.ru>
2016-01-28 14:31:41 +0300

    скрипты для использования fabric

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -8,6 +8,8 @@ from selenium import webdriver
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.webdriver.support.ui import WebDriverWait
 
+from .server_tools import reset_database
+
 
 class FunctionalTest(StaticLiveServerTestCase):
 
@@ -15,17 +17,23 @@ class FunctionalTest(StaticLiveServerTestCase):
     def setUpClass(cls):
         for arg in sys.argv:
             if 'liveserver' in arg:
-                cls.server_url = 'http://' + arg.split('=')[1]
+                cls.server_host = arg.split('=')[1]
+                cls.server_url = 'http://' + cls.server_host
+                cls.against_staging = True
                 return
         super().setUpClass()
+        cls.against_staging = False
         cls.server_url = cls.live_server_url
 
     @classmethod
     def tearDownClass(cls):
-        if cls.server_url == cls.live_server_url:
+        if not cls.against_staging:
             super().tearDownClass()
 
     def setUp(self):
+        if self.against_staging:
+            reset_database(self.server_host)
+
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
 
