340b68800322e0ac21451e5a16f9283fee5c683b
yurch <tesewar@mail.ru>
2016-02-03 12:01:05 +0300

    исправление юнит тестов для вьюхи Моих списков

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -6,6 +6,9 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 from django.utils.html import escape
 
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
 from lists.views import home_page
 from lists.models import Item, List
 from lists.forms import (
@@ -180,6 +183,13 @@ class NewItemTest(TestCase):
 class MyListsTest(TestCase):
 
     def test_my_lists_url_renders_my_lists_template(self):
+        User.objects.create(email='a@b.com')
         response = self.client.get('/lists/users/a@b.com/')
         self.assertTemplateUsed(response, 'my_lists.html')
+
+    def test_passes_correct_owner_to_template(self):
+        User.objects.create(email='wrong@owner.com')
+        correct_user = User.objects.create(email='a@b.com')
+        response = self.client.get('/lists/users/a@b.com/')
+        self.assertEqual(response.context['owner'], correct_user)
 
--- lists/views.py
+++ lists/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render, redirect
 from lists.models import List
 from lists.forms import ItemForm, ExistingListItemForm
 
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 # Create your views here.
 
@@ -32,5 +34,5 @@ def view_list(request, list_id):
 
 
 def my_lists(request, email):
-    return render(request, 'my_lists.html')
-
+    owner = User.objects.get(email=email)
+    return render(request, 'my_lists.html', {'owner': owner})
