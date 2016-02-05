4f50c45dea782107990fb762e7a7d5d2ffa387cc
yurch <tesewar@mail.ru>
2016-02-05 17:04:04 +0300

    тесты форм

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -1,8 +1,11 @@
+import unittest
+from unittest.mock import patch, Mock
+
 from django.test import TestCase
 
 from lists.forms import (
         DUPLICATE_ITEM_ERROR, EMPTY_ITEM_ERROR,
-        ExistingListItemForm, ItemForm
+        ExistingListItemForm, ItemForm, NewListForm
         )
 from lists.models import Item, List
 
@@ -56,4 +59,19 @@ class ExistingListItemFormTest(TestCase):
         form = ExistingListItemForm(for_list=list_, data={'text': 'hi'})
         new_item = form.save()
         self.assertEqual(new_item, Item.objects.all()[0])
+
+
+class NewListFormTest(unittest.TestCase):
+
+    @patch('lists.forms.List.create_new')
+    def test_save_creates_new_list_from_post_data_if_user_not_authenticated(
+        self, mock_List_create_new
+    ):
+        user = Mock(is_authenticated=lambda: False)
+        form = NewListForm(data={'text': 'new item text'})
+        form.is_valid()
+        form.save(owner=user)
+        mock_List_create_new.assert_called_once_with(
+            first_item_text='new item text'
+        )
 
