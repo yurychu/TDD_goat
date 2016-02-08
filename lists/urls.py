7f794389368d5add38ddbe9a292b60eff474a479
yurch <tesewar@mail.ru>
2016-02-08 18:00:01 +0300

    чистка экспериментальной вьюхи из тестов

--- lists/urls.py
+++ lists/urls.py
@@ -20,7 +20,7 @@ from lists import views
 
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
-    url(r'^new$', views.new_list2, name='new_list'),
+    url(r'^new$', views.new_list, name='new_list'),
     url(r'^(\d+)/$', views.view_list, name='view_list'),
     url(r'^users/(.+)/$', views.my_lists, name='my_lists'),
 ]
