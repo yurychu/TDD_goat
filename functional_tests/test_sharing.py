ae9235cf437b8b3ccaba6765f015b56a39fc7ec7
yurch <tesewar@mail.ru>
2016-02-10 15:00:13 +0300

    полный FT для шаринга списков

--- functional_tests/test_sharing.py
+++ functional_tests/test_sharing.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from .base import FunctionalTest
+from .home_and_list_pages import HomePage
 
 def quit_if_possible(browser):
     try: browser.quit()
@@ -22,12 +23,30 @@ class SharingTest(FunctionalTest):
 
         # Эдит переходит на домашнюю страницу и начинает список дел
         self.browser = edith_browser
-        self.browser.get(self.server_url)
-        self.get_item_input_box().send_keys('Get help\n')
+        list_page = HomePage(self).start_new_list('Get help')
 
         # Она помечает опцию "расшарить этот список"
-        share_box = self.browser.find_element_by_css_selector('input[name=email]')
+        share_box = list_page.get_share_box()
         self.assertEqual(
             share_box.get_attribute('placeholder'),
             'your-friend@example.com'
         )
+
+        # список становится расшаренными
+        # страница перезагружается и сообщается о расшаривании с Они
+        list_page.share_list_with('oniciferous@example.com')
+
+        # Они переходит на этот список
+        self.browser = oni_browser
+        HomePage(self).go_to_home_page().go_to_my_lists_page()
+
+        # И он видит что там написала Эдит
+        self.browser.find_element_by_link_text('Get help').click()
+
+        # И он может добавить туда еще что то
+        list_page.add_new_item('Hi Edith!')
+
+        # Когда Эдит обновляет свою страницу - она видит, что добавил Они
+        self.browser = edith_browser
+        self.browser.refresh()
+        list_page.wait_for_new_item_in_list('Hi Edith!', 2)
